c3093744eb11259d9d7b0fd66ce277f6b9e1576a
Administrator <Administrator@WIN-KLHCHF98M3B.local>
2020-11-13 19:41:42 -0600

    Cargando el proyecto de backend

---
+++ LinkedInBackend/LinkedInBackend/Controllers/LinkedInController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LinkedInBackend.Controllers
+{
+    [ApiController]
+    public class LinkedInController : ControllerBase
+    {
+        const string callbackAppUri = "dotnetconf";
+
+        [HttpGet]
+        [Route("dotnetconf/v1/ReceiveLinkedInResponse")]
+        public IActionResult ReceiveLinkedInResponse(string code)
+        {
+            string callbackUrl = $"{callbackAppUri}://#access_token={code}";
+            return Redirect(callbackUrl);
+        }
+    }
+}
